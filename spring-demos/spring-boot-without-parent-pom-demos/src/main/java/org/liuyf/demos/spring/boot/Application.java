b0eceef9d7a5e37f5189e185de9880d1d7eaad31
liuyf8688 <liuyf8688@126.com>
2015-10-01 15:14:30 +0800

    using YAML instead of Properties

--- spring-demos/spring-boot-without-parent-pom-demos/src/main/java/org/liuyf/demos/spring/boot/Application.java
+++ spring-demos/spring-boot-without-parent-pom-demos/src/main/java/org/liuyf/demos/spring/boot/Application.java
@@ -1,5 +1,6 @@
 package org.liuyf.demos.spring.boot;
 
+import org.liuyf.demos.spring.boot.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContextInitializer;
@@ -13,6 +14,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		
+		System.setProperty("spring.profiles.active", "production");
+		
 		SpringApplication application = new SpringApplication(Application.class);
 		application.addInitializers(new ApplicationContextInitializer<ConfigurableApplicationContext>() {
 
@@ -24,6 +27,16 @@ public class Application {
 		
 		ConfigurableApplicationContext ctx = application.run(args);
 		System.out.println("++++++++++ project.verion ++++++++: " + ctx.getBean(Version.class).getVersion());
+		
+		Config config = ctx.getBean(Config.class);
+		// servers
+		for (String server : config.getServers()) {
+			System.out.println("++++++++++ server ++++++++++: " + server);
+		}
+		// candidate servers
+		for (String server : config.getCandidateServers()) {
+			System.out.println("++++++++++ candidate server ++++++++++: " + server);
+		}
 		
 	}
 
