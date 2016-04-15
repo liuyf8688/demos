77cf84057142ea7f357ebcdcc6e36e12224aa1f4
liuyf8688 <liuyf8688@126.com>
2016-04-15 10:59:00 +0800

    rename the artifactId & adjust the project hierarchy

---
+++ spring-demos/spring-framework-demos/spring-framework-annotations-demos/src/test/java/com/liuyf/demos/spring/framework/annotations/test/SpringAnnotationTest.java
@@ -0,0 +1,62 @@
+package com.liuyf.demos.spring.framework.annotations.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.liuyf.demos.spring.framework.annotations.Config;
+import com.liuyf.demos.spring.framework.annotations.model.JdbcUserHolder;
+
+public class SpringAnnotationTest {
+
+	private AnnotationConfigApplicationContext context = null;
+	@Before
+	public void setUp() {
+		context = new AnnotationConfigApplicationContext();
+		context.register(Config.class);
+		context.refresh();
+		context.registerShutdownHook();
+	}
+	
+	@After
+	public void tearDown() {
+		context.close();
+	}
+	
+	@Test
+	public void loadDataFromPropertyFlieWithAnnotation() {
+		JdbcUserHolder holder = context.getBean(JdbcUserHolder.class);
+		assertEquals("liuyf", holder.getUserName());
+	}
+	
+	
+	@Test
+	public void loadDataFromPropertyFlieWithAnnotationExpectedSpecifyingValueIsMySQL1() {
+		JdbcUserHolder holder = context.getBean(JdbcUserHolder.class);
+		assertEquals("MySQL1", holder.getCurrentSQLType());
+	}
+	
+	@Test
+	public void loadDataFromPropertyFlieWithAnnotationExpectedInitArrays() {
+		JdbcUserHolder holder = context.getBean(JdbcUserHolder.class);
+		assertEquals("MySQL1", holder.getSupportSQLTypes().get(0));
+		assertEquals("SQLServer", holder.getSupportSQLTypes().get(1));
+	}
+
+	@Test
+	public void loadDataFromPropertyFlieWithAnnotationExpectedInitArraysUsingDefaultValue() {
+		JdbcUserHolder holder = context.getBean(JdbcUserHolder.class);
+		assertEquals("PostgrSQL", holder.getNoSupportSQLTypes().get(0));
+		assertEquals("HyperSQL", holder.getNoSupportSQLTypes().get(1));
+	}
+	
+	@Test
+	public void loadDataFromPropertyFlieWithAnnotationExpectedTimeoutIs1800() {
+		JdbcUserHolder holder = context.getBean(JdbcUserHolder.class);
+		assertEquals(1800, holder.getTimeout());
+	}
+
+}
---
+++ spring-demos/spring-framework-demos/spring-framework-web-app-demos/src/main/java/com/liuyf/demos/spring/framework/web/config/GlobalConfig.java
@@ -0,0 +1,25 @@
+package com.liuyf.demos.spring.framework.web.config;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class GlobalConfig {
+
+	@PostConstruct
+	public void init() {
+		System.out.println("=========================: " + getPropertySourcesBean());
+	}
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesBean() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+}
